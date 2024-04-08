9546f89e43e25668ede394dabc9f06403aa14bf7
Idevaldo Neto <inetomec@yahoo.com.br>
2024-04-08 09:00:49 -0300

    chore: Iniciado projeto da api

---
+++ pln-api/src/main/java/com/backend/plnapi/PlnApiApplication.java
@@ -0,0 +1,13 @@
+package com.backend.plnapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlnApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlnApiApplication.class, args);
+	}
+
+}
---
+++ pln-api/src/test/java/com/backend/plnapi/PlnApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.plnapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlnApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
