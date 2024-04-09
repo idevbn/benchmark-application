8007c9acb639306a3dff3705fde67bb0e012ad12
Idevaldo Neto <inetomec@yahoo.com.br>
2024-04-09 18:36:53 -0300

    feat: Criado controller para realizar requests à API externa

---
+++ pln-api/src/main/java/com/backend/plnapi/controllers/PlnApiCovidController.java
@@ -0,0 +1,40 @@
+package com.backend.plnapi.controllers;
+
+import com.backend.plnapi.clients.CovidApiClient;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
+@RestController
+public class PlnApiCovidController {
+
+    private final CovidApiClient covidApiClient;
+
+    public PlnApiCovidController(final CovidApiClient covidApiClient) {
+        this.covidApiClient = covidApiClient;
+    }
+
+    @GetMapping(value = "/covid")
+    public ResponseEntity<Object> getAllCovidData(
+            @RequestParam(required = false)
+            final String country,
+            @DateTimeFormat(pattern = "yyyy-MM-dd")
+            @RequestParam(required = false)
+            final LocalDate date,
+            @RequestParam(required = false)
+            final String region,
+            @RequestParam(required = false)
+            final String county,
+            @RequestParam(required = false)
+            final String type
+    ) {
+        return this.covidApiClient.getAllCovidData(
+                country, date, region, county, type
+        );
+    }
+
+}
