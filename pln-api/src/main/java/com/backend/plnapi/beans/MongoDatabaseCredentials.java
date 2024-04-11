3e08f093615ffeacdabc3452375315916e972fe4
Idevaldo Neto <inetomec@yahoo.com.br>
2024-04-11 10:04:10 -0300

    feat: Adicionados parâmetros à classe que instancia as credencias de acesso ao MongoDB

--- pln-api/src/main/java/com/backend/plnapi/beans/MongoDatabaseCredentials.java
+++ pln-api/src/main/java/com/backend/plnapi/beans/MongoDatabaseCredentials.java
@@ -17,13 +17,19 @@ public class MongoDatabaseCredentials {
     private final String database;
     private final String port;
     private final String hostname;
+    private final String password;
+    private final String username;
 
     public MongoDatabaseCredentials(@Value("${spring.data.mongodb.database}") final String database,
                                     @Value("${spring.data.mongodb.port}") final String port,
-                                    @Value("${spring.data.mongodb.host}")  final String hostname) {
+                                    @Value("${spring.data.mongodb.host}")  final String hostname,
+                                    @Value("${spring.data.mongodb.password}") final String password,
+                                    @Value("${spring.data.mongodb.username}") final String username) {
         this.database = database;
         this.port = port;
         this.hostname = hostname;
+        this.password = password;
+        this.username = username;
     }
 
 }
