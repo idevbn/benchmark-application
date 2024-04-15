8fee9d1ec8d58587114b1743b33be602df83a4ee
Idevaldo Neto <inetomec@yahoo.com.br>
2024-04-15 01:45:33 -0300

    feat: Adicionadas validações ao DTO

--- pln-api/src/main/java/com/backend/plnapi/dtos/in/BenchmarkInputDTO.java
+++ pln-api/src/main/java/com/backend/plnapi/dtos/in/BenchmarkInputDTO.java
@@ -1,20 +1,25 @@
 package com.backend.plnapi.dtos.in;
 
+import com.backend.plnapi.validators.BenchmarkNameAlreadyExistsValidator;
 import com.backend.plnapi.validators.CountryNamesMustBeDifferentValidator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
-import java.io.Serializable;
+import javax.validation.constraints.NotBlank;
 
 @Data
 @CountryNamesMustBeDifferentValidator
-public class BenchmarkInputDTO implements Serializable {
+public class BenchmarkInputDTO {
 
+    @BenchmarkNameAlreadyExistsValidator
+    @NotBlank(message = "{benchmark.validation.name}")
     private String benchmark;
 
+    @NotBlank(message = "{benchmark.validation.first-country.name}")
     @JsonProperty(value = "first_country_name")
     private String firstCountryName;
 
+    @NotBlank(message = "{benchmark.validation.second-country.name}")
     @JsonProperty(value = "second_country_name")
     private String secondCountryName;
 
