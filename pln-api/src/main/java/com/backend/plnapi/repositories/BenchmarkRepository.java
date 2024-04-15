d48d30d651e0144ddb005eececd2228f99b3cabd
Idevaldo Neto <inetomec@yahoo.com.br>
2024-04-15 01:40:43 -0300

    feat: Criado método que atualiza nome da benchmark

--- pln-api/src/main/java/com/backend/plnapi/repositories/BenchmarkRepository.java
+++ pln-api/src/main/java/com/backend/plnapi/repositories/BenchmarkRepository.java
@@ -1,7 +1,19 @@
 package com.backend.plnapi.repositories;
 
 import com.backend.plnapi.models.Benchmark;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface BenchmarkRepository  extends MongoRepository<Benchmark, String> {
+public interface BenchmarkRepository extends JpaRepository<Benchmark, Long> {
+
+    boolean existsByBenchmarkName(final String benchmarkName);
+
+    @Modifying
+    @Query(value = "UPDATE tb_benchmark SET benchmark_name = :benchmarkName WHERE id = :id",
+            nativeQuery = true)
+    void updateBenchmarkName(@Param("benchmarkName") final String benchmarkName,
+                             @Param("id") final Long id);
+
 }
