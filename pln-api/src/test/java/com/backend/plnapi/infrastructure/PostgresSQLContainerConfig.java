79cd2472ec19e18861999a70e8c37233a9dc851d
Idevaldo Neto <inetomec@yahoo.com.br>
2024-04-16 14:10:08 -0300

    test: Adicionados logs com informações sobre o container e o db de testes

--- pln-api/src/test/java/com/backend/plnapi/infrastructure/PostgresSQLContainerConfig.java
+++ pln-api/src/test/java/com/backend/plnapi/infrastructure/PostgresSQLContainerConfig.java
@@ -1,9 +1,11 @@
 package com.backend.plnapi.infrastructure;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Profile;
 import org.testcontainers.containers.PostgreSQLContainer;
 
+@Log4j2
 @Profile("test")
 @TestConfiguration
 public class PostgresSQLContainerConfig extends PostgreSQLContainer<PostgresSQLContainerConfig> {
@@ -28,6 +30,13 @@ public class PostgresSQLContainerConfig extends PostgreSQLContainer<PostgresSQLC
         System.setProperty("DB_URL", this.getJdbcUrl());
         System.setProperty("DB_USERNAME", this.getUsername());
         System.setProperty("DB_PASSWORD", this.getPassword());
+
+        log.info("DB_URL {}", this.getJdbcUrl());
+        log.info("DB_USERNAME {}", this.getUsername());
+        log.info("DB_PASSWORD {}", this.getPassword());
+        log.info("DB_NAME {}", this.getDatabaseName());
+        log.info("CONTAINER_ID {}", this.getContainerId());
+        log.info("CONTAINER_NAME {}", this.getContainerName());
     }
 
     @Override
